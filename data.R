eb07d70c83da8461ecfb40b7b4091060a6e1914b
kaikang <kangkai0714@gmail.com>
2025-04-12 11:12:02 -0400

    added data script

---
+++ data.R
@@ -0,0 +1,9 @@
+# play with data
+
+url <- "https://www.statlearning.com/s/Advertising.csv"
+Advertising <- read.csv(url)
+plot(Advertising$sales,Advertising$TV)
+
+
+model <- lm(sales ~ TV, data = Advertising)
+summary(model)
